e3a94a8c7ac5d1c956c7c7d4e613dcda52638d28
Isuru Umayanga <72663288+isuruwa@users.noreply.github.com>
2021-06-13 19:57:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
-cd
-pkg update
-pkg upgrade
-pkg install toilet
-cd TST
-pip install -r requirements.txt
-python tst.py
+
+    apt-get update -y
+    apt-get upgrade -y
+    apt install python -y
+    apt install python2 -y
+    apt install git -y
+    apt install ruby -y
+    gem install lolcat
+    apt install toilet
+    apt install fortune
+    apt install cowsay
+    git clone https://github.com/isuruwa/TST
+    cd SMS
+    pip install -r requirements.txt
+    python tst.py
